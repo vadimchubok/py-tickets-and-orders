4474299b18e5174739bba227f37d6dd7499c4733
Vadim <vadimchubok@gmail.com>
2025-12-03 17:23:34 +0200

    Sollution

--- db/models.py
+++ db/models.py
@@ -95,8 +95,11 @@ class Ticket(models.Model):
         ]
 
     def __str__(self) -> str:
-        return (f"{self.movie_session} "
-                f"(row: {self.row}, seat: {self.seat})")
+        return (
+            f"{self.movie_session.movie.title} "
+            f"{self.movie_session.show_time} "
+            f"(row: {self.row}, seat: {self.seat})"
+        )
 
     def clean(self) -> None:
         hall = self.movie_session.cinema_hall
